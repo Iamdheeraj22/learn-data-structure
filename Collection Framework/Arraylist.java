1596f8830abf9e5901417c2589b0afae7dacc1c7
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-03-02 22:50:09 +0530

    collection frmework

---
+++ Collection Framework/Arraylist.java
@@ -0,0 +1,15 @@
+/****
+ * Java ArrayList class uses a dynamic array for storing the elements. It is like an array, but there is no size limit. We can add or remove elements anytime.
+ * 
+ * The ArrayList in Java can have the duplicate elements also. It implements the List interface so we can use all the methods of List interface
+ * 
+ * It inherits the AbstractList class and implements List interface.
+ * 
+ * Java ArrayList class maintains insertion order.
+ * Java ArrayList allows random access because array works at the index basis.
+ */
+
+
+public class Arraylist {
+    
+}
