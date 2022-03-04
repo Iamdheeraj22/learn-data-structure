9d9fd3ce80eb25b70c28ad703d3e5b3694f6325e
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-03-04 23:40:19 +0530

    Brief discuss about Arraylist

--- Collection Framework/Arraylist.java
+++ Collection Framework/Arraylist.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /****
  * Java ArrayList class uses a dynamic array for storing the elements. It is like an array, but there is no size limit. We can add or remove elements anytime.
  * 
@@ -7,9 +8,85 @@
  * 
  * Java ArrayList class maintains insertion order.
  * Java ArrayList allows random access because array works at the index basis.
+ * 
+ * Java ArrayList class extends AbstractList class which implements List interface. The List interface extends the Collection
+and Iterable interfaces in hierarchical order.
+
+
+manipulation is little bit slower than the LinkedList in Java because a lot of shifting needs to occur if any element is removed from the array list.
+
+Java new generic collection allows you to have only one type of object in a collection. Now it is type safe so typecasting is not required at runtime.
+
+
  */
 
+ /****
+  * Ways to the iterate the elements of the collection in java
+
+        :- iterator interator 
+        :- for loop
+        :- for each loop
+        :- listiterator interface
+        :- for each() method
+        :- forEachRemaining
+  */
 
 public class Arraylist {
-    
+    public static void main(String[] args) {
+        List<String> array=new ArrayList<String>();
+        array.add("Dheeraj");
+        array.add("Dheeraj Prajapat");
+        array.add(1,"a");
+
+        //toArray()
+        array.toArray();
+
+        for(int i=0;i<array.size();i++){
+            System.out.println(array.get(i));
+        }
+
+        //Traversal through iterator
+       System.out.println("Print arraylist through Iterator....");
+       Iterator<String> iterator=array.listIterator();
+
+       while (iterator.hasNext()) {
+           System.out.println(iterator.next());
+       }
+       
+       //Remove the elements between given range
+       //Sort the arraylist elements 
+       
+       //Collections.sort(Array)
+
+
+        //Here, element iterates in reverse order through List iterator
+        ListIterator<String> listIterator=array.listIterator(array.size());
+        while(listIterator.hasPrevious()){
+            String str=listIterator.previous();
+            System.out.println(str);
+        }
+
+
+       //Remove the element into arraylist
+       array.remove(1);
+
+       array.remove("a");
+
+
+       //Clear elements
+       array.clear();
+
+       //Get the size
+       System.out.println(array.size());
+
+       ////check list empty 
+       System.out.println(array.isEmpty());
+
+       //Check the value is present in list or not
+       System.out.println(array.contains("o"));
+
+       //set the values at fixed position
+       array.set(0,"hjkh");
+       
+    }
 }
